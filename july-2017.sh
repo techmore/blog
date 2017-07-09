2ebd703cf1fc8da5daa79547a8db91027ab7d9a9
techmore <sdolbec1@gmail.com>
2017-07-09 19:18:16 -0400

    Update july-2017.sh

--- july-2017.sh
+++ july-2017.sh
@@ -6,7 +6,7 @@
 #blog.jpg
 wget https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjoptrBpf3UAhUBGD4KHewJCYYQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fwordpress-blog-post-cms-265132%2F&psig=AFQjCNH1OJoz3ednn8ycJoQHoZYS51syFA&ust=1499727241398609
 mv wordpress-265132_960_720.jpg blog.jpg
-sed -i.bak s/"\/w3images\/woods.jpg"/blog.jpg/g .html
+sed -i.bak s/"\/w3images\/woods.jpg"/blog.jpg/g index.html
 
 # BLOG TITLE
 sed -i.bak s/"TITLE HEADING"/"Day 1 Blogging"/g index.html
