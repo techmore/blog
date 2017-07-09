d5dc11b8c2a696fd4b33db8e628273df3c167267
techmore <sdolbec1@gmail.com>
2017-07-09 18:41:30 -0400

    Update blog.sh

--- blog.sh
+++ blog.sh
@@ -9,12 +9,11 @@ wget https://raw.githubusercontent.com/techmore/blog/master/index.html
 sed -i.bak s/"MY BLOG"/Techmore/g index.html
 sed -i.bak s/"Welcome to the blog of"//g index.html
 sed -i.bak s/unknown/"Get more\\!"/g index.html
-cd /var/share/nginx/html/w3schools
 
 # CREATE DOCKER FILE
 cat <<EOF >> Dockerfile 
 FROM nginx
-COPY static-html-directory /usr/share/nginx/html
+COPY blog /usr/share/nginx/html
 EOF
 
 docker build -t some-content-nginx .
