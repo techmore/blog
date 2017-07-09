bf5773a6248b3b6742eac2756ca973efab19aad8
techmore <sdolbec1@gmail.com>
2017-07-09 18:43:55 -0400

    Update blog.sh

--- blog.sh
+++ blog.sh
@@ -9,6 +9,7 @@ wget https://raw.githubusercontent.com/techmore/blog/master/index.html
 sed -i.bak s/"MY BLOG"/Techmore/g index.html
 sed -i.bak s/"Welcome to the blog of"//g index.html
 sed -i.bak s/unknown/"Get more\\!"/g index.html
+cd ..
 
 # CREATE DOCKER FILE
 cat <<EOF >> Dockerfile 
