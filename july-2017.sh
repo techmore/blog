06f496284f2151a0d6d2872cb1e0de5375c22848
techmore <sdolbec1@gmail.com>
2017-07-09 20:34:26 -0400

    Update july-2017.sh

--- july-2017.sh
+++ july-2017.sh
@@ -4,7 +4,7 @@
 
 # IMAGE TO REPLACE
 #blog.jpg
-wget https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjoptrBpf3UAhUBGD4KHewJCYYQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fwordpress-blog-post-cms-265132%2F&psig=AFQjCNH1OJoz3ednn8ycJoQHoZYS51syFA&ust=1499727241398609
+wget https://cdn.pixabay.com/photo/2014/02/13/07/28/wordpress-265132_960_720.jpg
 mv wordpress-265132_960_720.jpg blog.jpg
 sed -i.bak s/\/w3images\/woods.jpg/blog.jpg/g index.html
 
