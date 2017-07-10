2686f28ffb045917096698a4111421058f82afc5
techmore <sdolbec1@gmail.com>
2017-07-09 22:45:54 -0400

    Update july-2017.sh

--- july-2017.sh
+++ july-2017.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-#sed -i.bak s/"<\\!-- Blog entry 1 -->"/
-
 # IMAGE TO REPLACE
 #blog.jpg
 wget https://raw.githubusercontent.com/techmore/blog/master/blog.jpg
 sed -i.bak s=/w3images/woods.jpg=blog.jpg=g index.html
-
-# BLOG TITLE
-# sed -i.bak s/"TITLE HEADING"/"Day 1 Blogging"/g index.html
-    
-# Blog Description
-# sed -i.bak s/"Title description, "/"Creation of a reuseable blog, "/g index.html
-    
-# Date
-# sed -i.bak s/"April 7, 2014"/"July 7, 2017"/g index.html
-
-# Content
-# sed -i.bak s/"blog-post"/"I hate Macish."/g index.html
 
 cat <<EOF >> index.html
 
