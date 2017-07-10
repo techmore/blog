1357e76fbda3f153e28900a83e07aed7fa6bb645
techmore <sdolbec1@gmail.com>
2017-07-09 20:44:51 -0400

    Update july-2017.sh

--- july-2017.sh
+++ july-2017.sh
@@ -18,4 +18,4 @@ sed -i.bak s/"Title description, "/"Creation of a reuseable blog, "/g index.html
 sed -i.bak s/"April 7, 2014"/"July 7, 2017"/g index.html
 
 # Content
-# sed -i.bak s/"Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. "/"Is this working"/g
+sed -i.bak s/"Mauris neque quam, fermentum ut nisl vitae. "/"I hate Macish."/g
